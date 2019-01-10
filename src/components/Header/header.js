f4bd800ca37e9b5be04a9d63e9915a88cfce47cc
Dmitriy Feoktistov <feoktistovdmitriy@gmail.com>
2019-01-10 23:47:32 +0700

    fix: header refactor

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,29 +1,21 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import styles from './header.module.scss'
-import Navlinks from '../NavLinks/navlinks'
-import SidebarButton from '../Sidebar/sidebarButton'
+import React from 'react';
+import { Link } from 'gatsby';
+import styles from './header.module.scss';
+import Navlinks from '../NavLinks/navlinks';
+import SidebarButton from '../Sidebar/sidebarButton';
 
-const Header = props => {
-  let headerStyles
-  if (!props.floating) {
-    headerStyles = [styles.header]
-  } else {
-    headerStyles = [styles.header, styles.moving]
-  }
-  return (
-    <header className={headerStyles.join(' ')}>
-      <div className={styles.widthFix}>
-        <div className={styles.title}>
-          <Link to="/">Dima Feoktistov - Ph.D.</Link>
-        </div>
-        <nav className={styles.desktopOnly}>
-          <Navlinks />
-        </nav>
-        <SidebarButton drawerToggleClicked={props.drawerToggleClicked} />
+const Header = ({ drawerToggleClicked }) => (
+  <header className={styles.header}>
+    <div className={styles.widthFix}>
+      <div className={styles.title}>
+        <Link to="/">Dima Feoktistov - Ph.D.</Link>
       </div>
-    </header>
-  )
-}
+      <nav className={styles.desktopOnly}>
+        <Navlinks />
+      </nav>
+      <SidebarButton drawerToggleClicked={drawerToggleClicked} />
+    </div>
+  </header>
+);
 
-export default Header
+export default Header;
