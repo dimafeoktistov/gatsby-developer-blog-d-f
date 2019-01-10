98f7eb91ac7059a4bd2be4493be23098e3c102ea
Dmitriy Feoktistov <feoktistovdmitriy@gmail.com>
2019-01-10 22:02:48 +0700

    feat: added icons to navigation

--- src/components/NavLinks/navlinks.js
+++ src/components/NavLinks/navlinks.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import { FaHome, FaEnvelope, FaUser, FaBook } from 'react-icons/fa';
 import styles from './navlinks.module.scss';
 import Navlink from './NavLink/Navlink';
 
 const Navlinks = ({ clicked }) => (
   <ul className={styles.navigation}>
     <Navlink to="/" clicked={clicked}>
-        Home
+      <FaHome />
+      Home
     </Navlink>
     <Navlink to="/about" clicked={clicked}>
-        About
+      <FaUser />
+      About
     </Navlink>
     <Navlink to="/hire-me" clicked={clicked}>
-        Hire me
+      <FaEnvelope />
+      Hire me
     </Navlink>
     <Navlink to="/blog">
+      <FaBook />
       Blog
     </Navlink>
   </ul>
